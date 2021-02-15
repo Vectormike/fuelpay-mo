b0494af5a48621188cb6643fccf5a61ec13cc1e5
Vectormike <victorjonah199@gmail.com>
2021-02-16 00:09:37 +0100

    feat: set up home screen. WIP

---
+++ screens/welcome/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './style'
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
