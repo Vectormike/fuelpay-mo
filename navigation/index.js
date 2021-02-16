64133f398cd88eb214810596703b204290fc21a0
Vectormike <victorjonah199@gmail.com>
2021-02-16 11:35:08 +0100

    chore: update

--- navigation/index.js
+++ navigation/index.js
@@ -1,6 +1,7 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import Welcome from '../screens/welcome'
+import Login from '../screens/auth/login/index'
 
-const Navigator = createSwitchNavigator({ Welcome }, { initialRouteName: 'Welcome', headerMode: 'none' })
+const Navigator = createSwitchNavigator({ Welcome, Login }, { initialRouteName: 'Login', headerMode: 'none' })
 
 export default createAppContainer(Navigator)
