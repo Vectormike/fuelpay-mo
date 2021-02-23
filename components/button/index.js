813df25d66be6ace04c2b0652cf963e8b45bbcb7
Vectormike <victorjonah199@gmail.com>
2021-02-24 00:30:37 +0100

    feat(WIP): App button component

--- components/button/index.js
+++ components/button/index.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import { TouchableOpacity, Text } from 'react-native'
 import styles from './styles'
 
--- components/button/styles.js
+++ components/button/styles.js
@@ -8,13 +8,19 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingVertical: 10,
     paddingHorizontal: 12,
+    top: 273,
+    left: 100,
+    width: 340,
+    height: 55,
   },
   appButtonText: {
     fontSize: 18,
+    lineHeight: 22,
+    textAlign: 'center',
+    display: 'flex',
     color: '#fff',
+    fontStyle: 'normal',
     fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
   },
 })
 
